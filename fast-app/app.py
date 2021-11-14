5049ddeaf3b1065672a32c9af2fc035126d981ea
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2021-11-14 13:18:23 -0800

    get length of posted video for test

--- fast-app/app.py
+++ fast-app/app.py
@@ -5,23 +5,45 @@ from starlette.templating import _TemplateResponse
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
 from time import sleep
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+import shutil
+import cv2
 
 
-class Item(BaseModel):
-    num: int
+def save_upload_file_tmp(upload_file: UploadFile) -> Path:
+    try:
+        suffix = Path(upload_file.filename).suffix
+        with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
+            shutil.copyfileobj(upload_file.file, tmp)
+        tmp_path = Path(tmp.name)
+    finally:
+        upload_file.file.close()
+    return tmp_path
+
+
+def get_length_of_video(path: str) -> int:
+    cap = cv2.VideoCapture(path)                  # load video
+    video_frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)  # get num frame
+    video_fps = cap.get(cv2.CAP_PROP_FPS)                 # get frame rate
+    video_len_sec = video_frame_count / video_fps         # calc
+    return video_len_sec
 
 
 app = FastAPI()
 # for calling js under 'static/'
 app.mount("/static", StaticFiles(directory="static"), name="static")
 # for rendering index.html under 'templates/'
+
 templates = Jinja2Templates(directory="templates")
 
 
 @app.post("/api")
 def api(video: UploadFile = File(...)):
-    data = video.filename
-    return {"art": data}
+    tmp_path = save_upload_file_tmp(video)
+    tmp_path = str(tmp_path)
+
+    return {"art": get_length_of_video(tmp_path)}
 
 
 @app.get("/")
