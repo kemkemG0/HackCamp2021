cb2b577c3f9062354c7e04c43701fdf30d0500d1
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2021-11-14 14:14:20 -0800

    fix a little bit

---
+++ fast-app/imgProc/getFrame.py
@@ -0,0 +1,31 @@
+import cv2
+import os
+
+
+def save_all_frames(video_path, dir_path, basename, ext='jpg'):
+    cap = cv2.VideoCapture(video_path)
+    print(video_path)
+
+    if not cap.isOpened():
+        print('cannot open')
+        return
+
+    os.makedirs(dir_path, exist_ok=True)
+    base_path = os.path.join(dir_path, basename)
+
+    digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
+
+    n = 0
+
+    while True:
+        ret, frame = cap.read()
+        if ret and n % 5 == 0:
+            cv2.imwrite('{}_{}.{}'.format(
+                base_path, str(n).zfill(digit), ext), frame)
+        elif not ret:
+            return
+        n += 1
+
+
+save_all_frames('sampleVideos/sample_Trim.mp4',
+                'Output', 'sample_video_img')
