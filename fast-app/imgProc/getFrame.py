a940283cf4300017675029b6596fef3058d9af05
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2022-01-07 10:19:59 -0800

    add time calc

--- fast-app/imgProc/getFrame.py
+++ fast-app/imgProc/getFrame.py
@@ -1,10 +1,11 @@
 import cv2
 from imgProc.imgProc import convertToASCII
-
+import time
 outFiles = []
 
 
 def save_all_frames(video_path, dir_path='Output', ext='jpg'):
+    start_time = time.perf_counter()
     out = []
     cap = cv2.VideoCapture(video_path)
     print(video_path)
@@ -19,5 +20,8 @@ def save_all_frames(video_path, dir_path='Output', ext='jpg'):
         if ret and n % 5 == 0:
             out.append(convertToASCII(frame, n))
         elif not ret:
+            end_time = time.perf_counter()
+            elapsed_time = end_time - start_time
+            print(elapsed_time)
             return out
         n += 1
