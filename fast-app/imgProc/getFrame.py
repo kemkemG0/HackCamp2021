0d69d5a9c9a8db744b5527dd0293cbfc5cbb9fbb
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2022-01-07 10:26:26 -0800

    add time

--- fast-app/imgProc/getFrame.py
+++ fast-app/imgProc/getFrame.py
@@ -1,9 +1,11 @@
 import cv2
 from imgProc.imgProc import convertToASCII
 from multiprocessing import Pool
+import time
 
 
 def save_all_frames(video_path):
+    start_time = time.process_time()
     cap = cv2.VideoCapture(video_path)
     print(video_path)
 
@@ -20,5 +22,8 @@ def save_all_frames(video_path):
             print("elif")
             with Pool(10) as p:
                 ret = p.map(convertToASCII, frame_list)
+            end_time = time.process_time()
+            elapsed_time = end_time - start_time
+            print(elapsed_time)
             return ret
         n += 1
